296441de1d04c6cb95e3c941f2e1a1e89bcb59af
전동권 <jdkwon@jeondong-gwon-ui-noteubug.local>
2025-04-06 19:44:07 +0900

    basic

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,8 @@ from config import DATABASE_URI
 
 #Falsk 앱 초기화
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+CORS(app, supports_credentials=True)
+#CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -17,12 +18,7 @@ db.init_app(app)
 app.register_blueprint(api_bp)
 
 #SocketIO 초기화
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-# 클라이언트 연결 확인 로그
-@socketio.on('connect')
-def handle_connect():
-    print('✅ 클라이언트가 SocketIO에 연결되었습니다.')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
 # 데이터베이스 모델 객체 -> 테이블 생성
 with app.app_context():
@@ -33,20 +29,26 @@ import threading
 import time
 import random
 
-def simulate_monitoring_data():
+@socketio.on('connect')
+def handle_connect():
+    print('✅ 클라이언트가 SocketIO에 연결되었습니다.')
+
+
+def emit_data():
     while True:
-        sample_data = {
+        data = {
             "equipment_id": 1,
             "temperature": round(random.uniform(20.0, 40.0), 2),
             "voltage": round(random.uniform(3.5, 4.2), 2),
             "current": round(random.uniform(0.5, 2.0), 2),
             "timestamp": time.time()
         }
-        socketio.emit("monitor_data", sample_data)
-        print('📡 실시간 데이터 전송:', sample_data)
-        time.sleep(3)  # 3초 간격
+        socketio.emit("monitor_data", data)
+        print("📡 데이터 emit됨:", data)
+        time.sleep(1)
+
 
 if __name__ == "__main__":
-    threading.Thread(target=simulate_monitoring_data, daemon=True).start()
+    threading.Thread(target=emit_data, daemon=True).start()
     socketio.run(app, host='127.0.0.1', port=5000, debug=True)
     #app.run(debug=True, port=5000)
